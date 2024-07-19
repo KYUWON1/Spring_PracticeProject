0848b46e19241d7a6ed87bd308414a9104affd59
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-07-20 01:29:55 +0900

    AccountSystem:Fin

--- Project1 - AccountSystem/src/main/java/com/example/account/service/AccountService.java
+++ Project1 - AccountSystem/src/main/java/com/example/account/service/AccountService.java
@@ -29,8 +29,7 @@ public class AccountService {
      */
     @Transactional
     public AccountDto createAccount(Long userId, Long initialBalance) {
-        AccountUser accountUser = accountUserRepository.findById(userId)
-                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        AccountUser accountUser = getAccountUser(userId);
 
         validateCreateAccount(accountUser);
 
@@ -50,8 +49,7 @@ public class AccountService {
 
     @Transactional
     public AccountDto deleteAccount(Long userId,String accountNumber){
-        AccountUser accountUser = accountUserRepository.findById(userId)
-                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        AccountUser accountUser = getAccountUser(userId);
         Account account = accountRepository.findByAccountNumber(accountNumber)
                 .orElseThrow(() -> new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
 
@@ -67,14 +65,19 @@ public class AccountService {
 
     @Transactional
     public List<AccountDto> getAccountByUserId(Long userId){
-         AccountUser accountUser = accountUserRepository.findById(userId)
-                 .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        AccountUser accountUser = getAccountUser(userId);
 
         List<Account> accounts = accountRepository.findByAccountUser(accountUser);
 
         return accounts.stream()
                 .map(AccountDto::fromEntity)
                 .collect(Collectors.toList());
+    }
+
+    private AccountUser getAccountUser(Long userId) {
+        AccountUser accountUser = accountUserRepository.findById(userId)
+                .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
+        return accountUser;
     }
 
     private void validateCreateAccount(AccountUser accountUser){
