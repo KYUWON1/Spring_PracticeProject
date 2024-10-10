3e25d5d889573adb400fc8d1f6282eeb3a5ec128
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-10-10 19:23:28 +0900

    feat:openweathermap API 연결 테스트 및 테스트 Diary 도메인 생성

--- Project2 - WeatherDiary/src/main/java/com/example/weather/Repository/DiaryRepository.java
+++ Project2 - WeatherDiary/src/main/java/com/example/weather/Repository/DiaryRepository.java
@@ -1,7 +1,9 @@
 package com.example.weather.Repository;
 
+import com.example.weather.domain.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class DiaryRepository {
+public interface DiaryRepository extends JpaRepository<Diary,Integer> {
 }
--- Project2 - WeatherDiary/src/main/java/com/example/weather/controller/DiaryController.java
+++ Project2 - WeatherDiary/src/main/java/com/example/weather/controller/DiaryController.java
@@ -1,7 +1,26 @@
 package com.example.weather.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.weather.service.DiaryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
+@RequiredArgsConstructor
 public class DiaryController {
+    private final DiaryService diaryService;
+
+    @PostMapping("/create/diary")
+    // date는 여러형식으로 지정할 수 있어서, 포맷을 지정해주자.
+    public void createDiary(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+            @RequestBody String text
+    ){
+        diaryService.createDiary(date,text);
+    }
+
+
 }
