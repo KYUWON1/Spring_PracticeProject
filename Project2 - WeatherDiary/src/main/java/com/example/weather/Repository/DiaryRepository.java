016791b5bf9cb99db2e166c5d01118e03371522d
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-10-10 19:57:58 +0900

    feat:getDiaries API 추가

--- Project2 - WeatherDiary/src/main/java/com/example/weather/Repository/DiaryRepository.java
+++ Project2 - WeatherDiary/src/main/java/com/example/weather/Repository/DiaryRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary,Integer> {
     List<Diary> findAllByDate(LocalDate date);
+    List<Diary> findAllByDateBetween(LocalDate startDate,LocalDate endDate);
 }
