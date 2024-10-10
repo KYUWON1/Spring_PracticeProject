550e6fa9e3d373292b7d475d799275a010fbe41e
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-10-10 19:47:52 +0900

    feat:Diary Entity 추가 및 save 추가,fix: 날씨데이터 파싱 에러 수정

--- Project2 - WeatherDiary/src/main/java/com/example/weather/service/DiaryService.java
+++ Project2 - WeatherDiary/src/main/java/com/example/weather/service/DiaryService.java
@@ -1,5 +1,9 @@
 package com.example.weather.service;
 
+import com.example.weather.Repository.DiaryRepository;
+import com.example.weather.domain.Diary;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -15,7 +19,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class DiaryService {
+
+    private final DiaryRepository diaryRepository;
 
     @Value("${openweathermap.key}")
     private String apiKey;
@@ -26,7 +33,13 @@ public class DiaryService {
         // 받아온 날씨 데이터 파싱
         Map<String, Object> parsedData = parseWeather(weatherData);
         // 파싱된 데이터 + 일기 DB 삽입
-
+        Diary nowDiary = new Diary();
+        nowDiary.setDate(date);
+        nowDiary.setWeather(parsedData.get("main").toString());
+        nowDiary.setIcon(parsedData.get("icon").toString());
+        nowDiary.setTemperature((Double)parsedData.get("temp"));
+        nowDiary.setText(text);
+        diaryRepository.save(nowDiary);
     }
 
     private String getWeatherString(){
@@ -71,7 +84,9 @@ public class DiaryService {
 
         JSONObject mainData = (JSONObject) jsonObject.get("main");
         resultMap.put("temp",mainData.get("temp"));
-        JSONObject weatherData = (JSONObject) jsonObject.get("weather");
+        // 대괄호로 시작하면, JSON Array!
+        JSONArray weatherArray = (JSONArray) jsonObject.get("weather");
+        JSONObject weatherData = (JSONObject) weatherArray.get(0);
         resultMap.put("main",weatherData.get("main"));
         resultMap.put("icon",weatherData.get("icon"));
 
