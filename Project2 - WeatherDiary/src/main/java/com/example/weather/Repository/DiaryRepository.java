271512bf148613b633404396cc314853e4725c8f
KYUWON1 <127181209+KYUWON1@users.noreply.github.com>
2024-10-31 22:31:15 +0900

    feat:날씨일기 CRUD 생성

--- Project2 - WeatherDiary/src/main/java/com/example/weather/Repository/DiaryRepository.java
+++ Project2 - WeatherDiary/src/main/java/com/example/weather/Repository/DiaryRepository.java
@@ -3,6 +3,7 @@ package com.example.weather.Repository;
 import com.example.weather.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,4 +13,8 @@ public interface DiaryRepository extends JpaRepository<Diary,Integer> {
     List<Diary> findAllByDate(LocalDate date);
     List<Diary> findAllByDateBetween(LocalDate startDate,LocalDate endDate);
     Diary getFirstByDate(LocalDate date); // 맨 처음 1개를 가져옴
+    // Transactional 을 붙이지 않으면, 데이터가 삭제되지않는다 !
+    // db와 spring 사이에서 발생하는 예외에 대해서 처리를 해준다.
+    @Transactional
+    void deleteAllByDate(LocalDate date);
 }
--- Project2 - WeatherDiary/src/main/java/com/example/weather/controller/DiaryController.java
+++ Project2 - WeatherDiary/src/main/java/com/example/weather/controller/DiaryController.java
@@ -51,4 +51,12 @@ public class DiaryController {
         diaryService.updateDiary(date,text);
     }
 
+    @DeleteMapping("/delete/diary")
+    public void deleteDiary(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date
+    ){
+        diaryService.deleteDiary(date);
+    }
+
 }
